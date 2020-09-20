f34e23f36c87c1c9f1aaac6066671e40a0afc81a
t7gs <tigs4dev@gmail.com>
2020-09-20 20:25:29 -0300

    ajustando para erros netlify

--- front-web/src/components/Filters/index.tsx
+++ front-web/src/components/Filters/index.tsx
@@ -1,12 +1,12 @@
-import React, { Props } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-type Props1 = {
+type Props = {
     link: string;
     linkText: string;
 }
 
-const Filters = ({link, linkText}: Props1) => (
+const Filters = ({link, linkText}: Props) => (
     <div className="filters-container records-actions">
         <Link to={link}>
             <button className="action-filters">
